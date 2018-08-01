8a61c496f832f563a6e29f36d1a3410be4915ef3
Matias Piipari <matias.piipari@gmail.com>
2018-08-01 01:52:17 +0100

    Drops .eslintrc.js

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "extends": "standard",
-    "rules": {
-        "indent": 0,
-        "quotes": 0,
-        "one-var": 0,
-        "space-before-function-paren": 0
-    }
-};
